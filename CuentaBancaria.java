f9644943f899fc58ad33a09961fd7f270379ccd2
Cinthia Carreon <cbi2243051516@izt.uam.mx>
2026-06-08 18:55:01 +0000

    Cremoas cuenta bancaria implementando la interfaz

---
+++ CuentaBancaria.java
@@ -0,0 +1,40 @@
+public class CuentaBancaria implements Transaccionable {
+
+    protected double saldo;
+    protected String numeroCuenta;
+
+    public CuentaBancaria(String numeroCuenta, double saldo) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+
+    public double consultarSaldo() {
+        return saldo;
+    }
+
+    public String obtenerNumero() {
+        return numeroCuenta;
+    }
+
+    @Override
+    public void depositar(double monto) {
+        if (monto > 0) {
+            saldo += monto;
+        }
+    }
+
+    @Override
+    public void retirar(double monto) {
+        if (monto > 0 && monto <= saldo) {
+            saldo -= monto;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaBancaria{" +
+                "numeroCuenta='" + numeroCuenta + '\'' +
+                ", saldo=" + saldo +
+                '}';
+    }
+}
