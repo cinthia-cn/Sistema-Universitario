bc5ad4a8448ab7a05a537a8a13d9d1db63853e48
Cinthia Carreon <cbi2243051516@izt.uam.mx>
2026-06-08 18:58:49 +0000

    Creamos Cuenta corriente con la herencia de cuenta bancaria

---
+++ CuentaCorriente.java
@@ -0,0 +1,25 @@
+public class CuentaCorriente extends CuentaBancaria {
+
+    public CuentaCorriente(String numeroCuenta, double saldo) {
+        super(numeroCuenta, saldo);
+    }
+
+    public void girarCheque(double monto) {
+        retirar(monto);
+        System.out.println("Cheque girado por $" + monto);
+    }
+
+    public void cobrarComision() {
+        double comision = 50.0;
+        saldo -= comision;
+        System.out.println("Comisión cobrada: $" + comision);
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaCorriente{" +
+                "numeroCuenta='" + numeroCuenta + '\'' +
+                ", saldo=" + saldo +
+                '}';
+    }
+}
