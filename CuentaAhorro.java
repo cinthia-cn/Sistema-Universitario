810395788566568e61ccd85210d8796ad93a284d
Cinthia Carreon <cbi2243051516@izt.uam.mx>
2026-06-08 19:01:36 +0000

    Creamos Cuenta ahorro

---
+++ CuentaAhorro.java
@@ -0,0 +1,25 @@
+public class CuentaAhorro extends CuentaBancaria {
+
+    private double montoLimite;
+
+    public CuentaAhorro(String numeroCuenta,
+                        double saldo,
+                        double montoLimite) {
+        super(numeroCuenta, saldo);
+        this.montoLimite = montoLimite;
+    }
+
+    public double calcularIntereses() {
+        double tasa = 0.05; // 5%
+        return saldo * tasa;
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaAhorro{" +
+                "numeroCuenta='" + numeroCuenta + '\'' +
+                ", saldo=" + saldo +
+                ", montoLimite=" + montoLimite +
+                '}';
+    }
+}
