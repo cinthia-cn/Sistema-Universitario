f7a89470cc2cccb824ab64729ed31372fc54c8a5
Cinthia Carreon <cbi2243051516@izt.uam.mx>
2026-06-26 05:06:03 +0000

    Corregi insconsisteccias de trabajador, se le cambio cuenta corriente y metodos

--- sistemasuniversitarios/Trabajador.java
+++ sistemasuniversitarios/Trabajador.java
@@ -1,17 +1,26 @@
 package sistemasuniversitarios;
-import banco.CuentaCorriente;
 
+import banco.CuentaCorriente;
 import java.time.LocalDate;
 import java.time.Period;
 
-public class Trabajador extends persona {
+public class Trabajador extends Persona {
 
 
 //ATRIBUTOS
-private CuentaCorriente cuenta; //Atributo de la nueva practica
+private CuentaCorriente[] cuentas; 
+//SE VOLVIO A MODIFICAR A UN ARREGLO 
+// PQ UN TRABAJADOR PUEDE EXISTIR SI TIENE 0, 1 O MAS CUENTAS
+// POR LO TANTO ES UNA AGREGACION, Y SE PUSIMOS UN ARREGLO DE CUENTAS
+private static final int MAX_CUENTAS = 3;
+private int totalCuentas;
+//AQUI LE AGREGAMOS ESTOS ATRIBUTOS PQ
+//EN MI ARREGLO YO PUSE 3 CUENTAS, SOLO POR TRABAJAR CON UN  NUMERO
+// SI EL TRABAJADOR QUIERE TENER MAS DE 3 CUENTAS, SOLO SE CAMBIA EN NUMERO
+// DECLARE UN TOTAL DE CUENTAS ESTE AYUDARA  A SABER EL NUMERO DE CUENTAS QUE SE TIENE Y SABER SI SE TIENE O QUIERE O NECESITA AGREGAR MAS CUENTAS
+// SI ES QUE ASI LO DESEARA EL TRABAJADOR. 
 
 private String identificador; // Agregamos atributo faltante para mejorar la presentacion de busqueda recursiva
-
 private LocalDate fechaIngreso;
 private double salario;
 private String puesto;
@@ -23,15 +32,20 @@ public Trabajador (LocalDate fechaIngreso,
 		String puesto, 
 		String nombre, 
 		LocalDate fechaNacimiento, 
-		String direccion,CuentaCorriente cuenta){
+		String direccion,
+		){
+//INVOCAMOS CONSTRUCTORES DE PERSONA
         super(nombre, fechaNacimiento, direccion);
 	this.identificador = identificador;        
 	this.fechaIngreso = fechaIngreso;
         this.puesto = puesto;
         this.salario = salario;
-	this.cuenta= cuenta;
-	
+
+	cuentas = new CuentaCorriente[MAX_CUENTAS];
+	totalCuentas = 0;
     }
+
+// CONTRUCTOR VACIO
 
 public Trabajador() {
 
@@ -41,17 +55,22 @@ public Trabajador() {
     this.salario = 0;
     this.puesto = "";
     this.identificador = "";
+   
+    cuentas = new CuentaCorriente[MAX_CUENTAS];
+    totalCuentas = 0;
 }
 
 
-//Setter y getter de CUENTA CORRIENTE
-public CuentaCorriente getCuenta(){
-	return cuenta;
+//GETTERS de CUENTA CORRIENTE
+// EN ESTE CASO NO TIENE SETTER PQ YA NO APLICA POR SER VARIAS CUENTAS
+public CuentaCorriente[] getCuentas(){
+	return cuentas;
 }
 
-public void setCuenta(CuentaCorriente cuenta){
-	this.cuenta=cuenta;
+public int getTotalCuentas(){
+	retutn totalCuentas;
 }
+
 
 //Setter y getter de IDENTIFICADOR
 public String getIdentificador(){
@@ -85,6 +104,52 @@ public void setPuesto(String puesto){
         return puesto;
     }
 
+// METODO DE CUENTA CORRIENTE CUENTAS
+public boolean agregarCuenta(CuentaCorriente cuenta) {
+
+        if (totalCuentas < MAX_CUENTAS) {
+
+            cuentas[totalCuentas] = cuenta;
+            totalCuentas++;
+
+            return true;
+        }
+	return false;
+    }
+
+
+//AGREGAMOS ESTE METODO PARA LA BUSQUEDA LINEAL
+
+    public CuentaCorriente buscarCuenta(String numeroCuenta) {
+
+        for (int i = 0; i < totalCuentas; i++) {
+
+            if (cuentas[i].obtenerNumero().equals(numeroCuenta)) {
+                return cuentas[i];
+            }
+
+        }
+
+        return null;
+ }
+
+    public void mostrarCuentas() {
+
+        if (totalCuentas == 0) {
+
+            System.out.println("No tiene cuentas registradas.");
+            return;
+
+        }
+
+        for (int i = 0; i < totalCuentas; i++) {
+
+            System.out.println(cuentas[i]);
+
+        }
+}
+
+
 //Metodo  CALCULAR ANTIGUEDAD
 public int calcularAntiguedad(){
 	return Period.between(fechaIngreso,
@@ -104,7 +169,7 @@ public String toString(){
 	"\nFecha de ingreso: " + fechaIngreso +
 	"\nSalario: " +salario +
 	"\nPuesto: " + puesto +
-	"\nCuenta: " + cuenta +
+	"\nTotal de Cuentas: " + totalCuentas +
 	"\nIdentificador:" +identificador;
 }
 
